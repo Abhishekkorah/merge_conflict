999833177623745144270178ab9ca84e4939465a
KORAH <korahabhishek@gmail.com>
2021-09-03 10:44:43 +0530

    Another Second Commit

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,11 @@
 class Calculator{
 	
 	public static void add(int firstNumber, int secondNumber) {
-		System.out.println("Adding two numbers: "+((4*firstNumber) + (4*secondNumber)));
+		System.out.println("Adding two numbers: "+((6*firstNumber) + (6*secondNumber)));
 	}
 	
 	public static void difference(int firstNumber, int secondNumber) {
-		System.out.println("Difference of two numbers: " +((4*firstNumber) - (4*secondNumber)));
+		System.out.println("Difference of two numbers: " +((6*firstNumber) - (6*secondNumber)));
 	}
 	public static void main(String[] args) {
 		
