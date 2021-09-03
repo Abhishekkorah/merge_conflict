5a4a30a888a614a0c82d58cf5e6f1d915af1ff4f
KORAH <korahabhishek@gmail.com>
2021-09-03 10:09:47 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+class Calculator{
+	
+	public static void add(int firstNumber, int secondNumber) {
+		System.out.println("Adding two numbers: "+(firstNumber + secondNumber));
+	}
+	
+	public static void difference(int firstNumber, int secondNumber) {
+		System.out.println("Difference of two numbers: " +(firstNumber - secondNumber));
+	}
+	public static void main(String[] args) {
+		
+		add(2,3);	
+		difference(10,2);
+	}
+	
+	
+}
